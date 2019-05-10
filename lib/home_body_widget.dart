d1bc4697fd8d577945dae3302fa9df7f8045d2f8
Harry Woodworth <harryawoodworth@gmail.com>
2019-05-09 22:21:17 -0400

    Move Sand Gen to it's own function

--- lib/home_body_widget.dart
+++ lib/home_body_widget.dart
@@ -1,57 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:time_scribe/main.dart';
 
-class ElapsedTime {
-  final int seconds;
-  final int minutes;
-
-  ElapsedTime({
-    this.seconds,
-    this.minutes,
-  });
-}
-
-class Dependencies {
-  final List<ValueChanged<ElapsedTime>> timerListeners = <ValueChanged<ElapsedTime>>[];
-  final TextStyle textStyle = const TextStyle(fontSize: 40.0, fontFamily: "Bebas Neue");
-  final Stopwatch stopwatch = new Stopwatch();
-  final int timerMillisecondsRefreshRate = 30;
-}
-
 class HomeBodyWidget extends StatefulWidget {
   HomeBodyWidgetState createState() => new HomeBodyWidgetState();
 }
 
 class HomeBodyWidgetState extends State<HomeBodyWidget> {
 
-  static final Dependencies dependencies = new Dependencies();
-
   @override
   Widget build(BuildContext context) {
     var timerService = TimerService.of(context);
     return Column(
       children: <Widget>[
-        timerSection,
+        Container( // Top row containing stopwatch
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget> [
+              Column(
+                children: <Widget> [
+                  Icon(
+                   Icons.account_balance,
+                   color: Colors.black87,
+                  ),
+                  Text('Wow'),
+                ],
+              ),
+              AnimatedBuilder(
+                animation: timerService,
+                builder: (context, child) {
+                  return Row (
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget> [
+                      Text('${timerService.currentDuration.inSeconds}'),
+                      FlatButton(
+                        color: Colors.white,
+                        onPressed: !timerService.isRunning ? timerService.start : timerService.stop,
+                        child: Icon(!timerService.isRunning ? Icons.play_arrow : Icons.stop),
+                      ),
+                    ],
+                  );
+                }
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
-
-  Widget timerSection = Container(
-
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget> [
-        Icon(
-          Icons.account_balance,
-          color: Colors.black87,
-        ),
-        Text('Wow'),
-        Icon(
-          dependencies.stopwatch.isRunning ? Icons.play_arrow: Icons.stop,
-          color: dependencies.stopwatch.isRunning ? Colors.green : Colors.red,
-        ),
-      ],
-    ),
-  );
 
 }
